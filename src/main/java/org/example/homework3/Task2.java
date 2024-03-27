e32b00839e7c476c6b0b38c40e2e6a2594c31a55
Kyrylo Sazonov <skai220891@gmail.com>
2024-03-28 01:01:33 +0200

    Homework3 task 2

---
+++ src/main/java/org/example/homework3/Task2.java
@@ -0,0 +1,16 @@
+package org.example.homework3;
+
+public class Task2 {
+    public static void main(String[] args) {
+        int a=2;
+        int b=3;
+
+        //Minimum value
+        int min = Math.min(a,b);
+        System.out.println("Minimum value: " + min);
+
+        //Maximum value
+        int max = Math.max(a,b);
+        System.out.println("Maximum value: " + max);
+    }
+}
