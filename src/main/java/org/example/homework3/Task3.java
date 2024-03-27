8bcf98bf007bd17e34faf47d525b4de6a6a5d9ac
Kyrylo Sazonov <skai220891@gmail.com>
2024-03-28 01:08:18 +0200

    Homework3 task 3

--- src/main/java/org/example/homework3/Task3.java
+++ src/main/java/org/example/homework3/Task3.java
@@ -1,2 +1,18 @@
-package org.example.homework3;public class Task3 {
+package org.example.homework3;
+
+public class Task3 {
+    public static void main(String[] args) {
+
+        //A long variable
+        long a = 1234567890251351234L;
+
+        // Print the original long value
+        System.out.println("Original long value: " + a);
+
+        // Convert the long to int (possible loss of precision if long value exceeds int range)
+        int b = (int) a;
+
+        // Print the converted int value
+        System.out.println("Converted int value: " + b);
+    }
 }
