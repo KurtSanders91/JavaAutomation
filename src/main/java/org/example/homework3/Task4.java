9f2cc42ac9a9b6a2b2eb55c8ccb9d18391755dc4
Kyrylo Sazonov <skai220891@gmail.com>
2024-03-28 01:16:51 +0200

    Homework3 task 4

---
+++ src/main/java/org/example/homework3/Task4.java
@@ -0,0 +1,20 @@
+package org.example.homework3;
+
+public class Task4 {
+    public static void main(String[] args) {
+
+        // Declare and initialize two integer variables
+        int dividend_A = 7;
+        int divisor_B = 5;
+
+        // Calculate the integer part of the division
+        int integerPart = dividend_A / divisor_B;
+
+        // Calculate the remainder after division
+        int remainder = dividend_A % divisor_B;
+
+        // Print the integer part and remainder
+        System.out.println("Integer part of division: " + integerPart);
+        System.out.println("Remainder after division: " + remainder);
+    }
+}
