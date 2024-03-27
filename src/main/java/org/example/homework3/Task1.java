6e0b9b6252b0556e24d920510c4451cec39a4949
Kyrylo Sazonov <skai220891@gmail.com>
2024-03-28 00:48:51 +0200

    Homework3 task 1

---
+++ src/main/java/org/example/homework3/Task1.java
@@ -0,0 +1,30 @@
+package org.example.homework3;
+
+public class Task1 {
+    public static void main(String[] args) {
+        int a = 7;
+        int b = 77;
+
+        //Sum
+        int sum = a+b;
+        //Print to console
+        System.out.println("Sum: " + sum);
+
+        //Substraction
+        int subtraction = a-b;
+        //Print to console
+        System.out.println("Subtraction: " + subtraction);
+
+        //Multiplication
+        int multiplication = a*b;
+        //Print to console
+        System.out.println("Multiplication: " + multiplication);
+
+        //Division
+        double division = (double) a/b;
+        //Print to console
+        System.out.println("Division: " + division);
+
+
+    }
+}
