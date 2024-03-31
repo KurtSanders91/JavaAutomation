2b53da728d25de10224dda6048d2c14141b0a2e7
Kyrylo Sazonov <skai220891@gmail.com>
2024-03-31 21:20:00 +0300

    Homework3 task 5

---
+++ src/main/java/org/example/homework4/Task2.java
@@ -0,0 +1,29 @@
+package org.example.homework4;
+
+public class Task2 {
+          public static void printYearsAndDays(long minutes) {
+            if (minutes < 0) {
+                System.out.println("Invalid Value");
+                return;
+            }
+
+            long minutesInHour = 60;
+            long hoursInDay = 24;
+            long daysInYear = 365;
+
+            long totalMinutes = minutes;
+            long totalHours = totalMinutes / minutesInHour;
+            long totalDays = totalHours / hoursInDay;
+            long years = totalDays / daysInYear;
+            long remainingDays = totalDays % daysInYear;
+
+            System.out.println(minutes + " min = " + years + " y and " + remainingDays + " d");
+        }
+
+        public static void main(String[] args) {
+            printYearsAndDays(525600); // 1 year
+            printYearsAndDays(1051200); // 2 years
+            printYearsAndDays(1440); // 0 years, 1 day
+            printYearsAndDays(-100); // Invalid Value
+        }
+    }
