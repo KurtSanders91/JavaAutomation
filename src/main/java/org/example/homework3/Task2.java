14f1a4c5fa7cdd3cd98b459737142e47e074d812
KurtSanders91 <49753074+KurtSanders91@users.noreply.github.com>
2024-03-31 21:47:44 +0300

    Update Task2.java

--- src/main/java/org/example/homework3/Task2.java
+++ src/main/java/org/example/homework3/Task2.java
@@ -5,11 +5,9 @@ public class Task2 {
         int a=2;
         int b=3;
 
-        //Minimum value
         int min = Math.min(a,b);
         System.out.println("Minimum value: " + min);
 
-        //Maximum value
         int max = Math.max(a,b);
         System.out.println("Maximum value: " + max);
     }
