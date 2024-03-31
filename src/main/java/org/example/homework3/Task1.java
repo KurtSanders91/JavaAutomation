8ce6c5b78a8659ebc3720e050d28e27a6e9df62d
KurtSanders91 <49753074+KurtSanders91@users.noreply.github.com>
2024-03-31 21:47:19 +0300

    Update Task1.java

--- src/main/java/org/example/homework3/Task1.java
+++ src/main/java/org/example/homework3/Task1.java
@@ -5,24 +5,16 @@ public class Task1 {
         int a = 7;
         int b = 77;
 
-        //Sum
         int sum = a+b;
-        //Print to console
         System.out.println("Sum: " + sum);
 
-        //Substraction
         int subtraction = a-b;
-        //Print to console
         System.out.println("Subtraction: " + subtraction);
 
-        //Multiplication
         int multiplication = a*b;
-        //Print to console
         System.out.println("Multiplication: " + multiplication);
 
-        //Division
         double division = (double) a/b;
-        //Print to console
         System.out.println("Division: " + division);
 
 
